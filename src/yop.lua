44e0d772f72d62955d613cfb8720c241194bc01c
Gauthier Billot <gauthier.billot@fishingcactus.com>
2017-06-27 13:41:10 +0200

    Wip

--- src/yop.lua
+++ src/yop.lua
@@ -1,5 +1,10 @@
 require("os")
 
 all_args = table.concat(arg, " ")
-
-os.execute("yaourt " .. all_args)
+if arg[1]:sub(1, 1) == '-' then
+    for i=2,#arg do
+        os.execute("yaourt " .. arg[1] .. " " .. arg[i])
+    end
+else
+    os.execute("yaourt " .. table.concat(arg, " "))
+end
